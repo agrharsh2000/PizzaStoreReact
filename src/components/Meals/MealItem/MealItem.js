5022c8bcc61eeea797e3692e83243879e6e7d475
Harsh <agr.harsh.2000@gmail.com>
2023-05-10 08:11:58 +0530

    adding cart component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -10,7 +10,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{props.price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
